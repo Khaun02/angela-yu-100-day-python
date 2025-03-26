a2f75c1385c7d24c4ebb51ae59028afeabcdba1d
Khaun02 <khaunx02@gmail.com>
2025-03-25 22:33:33 -0500

    feat: first day learning
    - learned print statements
    - learned string manipulation
    * string new line and concatenation

---
+++ day001/task.py
@@ -0,0 +1,12 @@
+# known as string; "string of characters"
+# should be new line at end of file for good practice
+print("Hello World!" + " I am a string")
+
+# STRING MANIPULATION PART
+
+# new line it's "\n"
+# dont even need ""
+print("Hello World!\nHello World")
+
+# concatenation
+print("Hello" + " " + "Shaun Khang")
