f38012e05a14c91cda81aaf801c01ee9ba58519b
Khaun02 <khaunx02@gmail.com>
2025-04-01 22:54:00 -0500

    feat: continuation of day002

---
+++ day002/dataTypePrac.py
@@ -0,0 +1,18 @@
+# primitive data types
+
+# this is how to get the index of the string
+    # subscripting
+    # can also use - number for index, -1 means the last letter in the string; useful couunting backwards
+print("Hello"[3])
+
+# to declare integer type
+
+# large integers
+print(123_456_789)
+
+# Float = floating point number; think of decimal point as floating around the  number
+print(3.14)
+
+# boolean
+    # only True/False
+
