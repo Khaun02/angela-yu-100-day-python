a1eb9e06ba4770b987f890345594dbea4efa5fd3
Khaun02 <khaunx02@gmail.com>
2025-04-04 10:53:25 -0500

    feat: introduction of day003
    - learned about if statements and their syntax
    - started on modulo

---
+++ day003/controlFlow.py
@@ -0,0 +1,11 @@
+# 22. Control Flow with if / else and Conditional Operators
+
+# Roller Coaster Program
+
+print("\nWelcome to the rollercoaster!")
+height = int(input("What is your height in cm: "))
+
+if height >= 120:
+    print("You can ride this rollercoaster!")
+else:
+    print("Your too short!  You cannot ride this rollercoaster!\n")
---
+++ learnModulo.py
@@ -0,0 +1,1 @@
+# 23. Introducing the Modulo
