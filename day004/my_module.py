3b9149428f2769aa8c23dd443b3005f4a56c1404
Khaun02 <khaunx02@gmail.com>
2025-04-16 00:05:22 -0500

    feat: starting day004
    - learned about the random module and how to use it
    - learned how to create our own modules and use their variables

---
+++ day004/my_module.py
@@ -0,0 +1,3 @@
+# 31. Random Module; learning how to create our own module
+
+my_favorite_number = 3.1415
---
+++ day004/randomMod.py
@@ -0,0 +1,6 @@
+# 31. Random Module
+
+import random
+
+# random_integer = random.randint(1, 10)
+# print(f"\n{random_integer}")
