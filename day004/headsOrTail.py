0228f2c2314f67bba9f6cb1fc619a901fdc2eb7b
Khaun02 <khaunx02@gmail.com>
2025-04-21 23:25:44 -0500

    feat: contin of day004
    - learned more on random module
    - learned how to make/use my own modules

---
+++ day004/headsOrTail.py
@@ -0,0 +1,9 @@
+# 31 Challenge: Heads or Tail
+
+import random
+
+choice = random.randint(1,2)
+if choice == 1:
+    print("\nHeads")
+else:
+    print("\nTails")
--- day004/randomMod.py
+++ day004/randomMod.py
@@ -4,3 +4,10 @@ import random
 
 # random_integer = random.randint(1, 10)
 # print(f"\n{random_integer}")
+
+# from 0-<10
+#   rand_number_0_1 = random.random() * 10
+#   print(rand_number_0_1)
+
+random_float = random.uniform(1,10)
+print(random_float)
